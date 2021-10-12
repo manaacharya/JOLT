2ae8d46a4fcd95a5700c7e8132710a0e009b4b29
Mustafa <m-thuraya@digitalfuture.sa>
2021-10-11 19:40:17 -0700

    I have done changes to indexcshtml.cs and error.cshtml.cs

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-
+//im changing
 namespace ContosoCrafts.WebSite.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
