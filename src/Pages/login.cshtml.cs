7ee0b697bb39d525855566780d0dc168fa0a5a03
Fangsheng Xu <fangcyxu@uw.edu>
2021-10-16 23:56:13 -0700

    FX: added basic contents for Login.cshtml and Login.cshtml.cs
    question: how come these is not reaction after clicking "login" in homescreen

--- src/Pages/login.cshtml.cs
+++ src/Pages/login.cshtml.cs
@@ -2,15 +2,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
     public class LoginModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<IndexModel> _logger;
+
+        public LoginModel(ILogger<IndexModel> logger,
+            JsonFileUserService userService)
         {
+            _logger = logger;
+            UserService = userService;
         }
+
+        public JsonFileUserService UserService { get; }
+        public IEnumerable<UserModel> UserList { get; private set; }
+        public void OnGet() => UserList = UserService.GetUsers();
+
+        public string CurrentUsername { get; set; }
+        public string CurrentPassword { get; set; }
+
+        public Boolean isValidUser()
+        {
+            return false;
+        }
+
+
     }
 }
