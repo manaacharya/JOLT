a9968636f5dcf05ad4aed05777dee1357685acf7
manasaacharya <macharya@seattleu.edu>
2021-10-19 12:09:43 -0700

    MBA: PollsPage.cshtml and .cs created a polls page that will display polls in the future.

---
+++ src/Pages/PollsPage.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class PollsPageModel : PageModel
+    {
+        private readonly ILogger<PollsPageModel> _logger;
+
+        public PollsPageModel(ILogger<PollsPageModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
