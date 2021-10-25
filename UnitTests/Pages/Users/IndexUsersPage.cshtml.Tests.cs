26bdecaefe25c3ef315f853a37326bdfd7fc08f8
Edwin Kaburu <kaburuedwin123@gmail.com>
2021-10-25 12:52:45 -0700

    Created and Added unit tests in IndexUserPage.cshtml.Tests.cs, in the indexing of Users with 100% code coverage for getting Users in the UserPage

---
+++ UnitTests/Pages/Users/IndexUsersPage.cshtml.Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using Moq;
+
+using ContosoCrafts.WebSite.Pages;
+
+namespace UnitTests.Pages.Users
+{
+    class IndexUsersPage
+    {
+        #region TestSetup
+
+        public static UsersPageModel usersPageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<UsersPageModel>>();
+
+            usersPageModel = new UsersPageModel(MockLoggerDirect, TestHelper.UserService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Users()
+        {
+            // Arrange
+
+            // Act
+            usersPageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, usersPageModel.ModelState.IsValid);
+            Assert.AreEqual(true, usersPageModel.Users.ToList().Any());
+        }
+        #endregion OnGet
+
+    }
+}
