7b4be437383d9e9986e2b08face71e358a07e809
FangshengXuaJason <xufangsheng@seattleu.edu>
2021-10-26 12:33:15 -0700

    FX: renamed LoginModel.cs to UserLoginModel.cs under src\Models

--- UnitTests/Pages/Users/Login.cshtml.Test.cs
+++ UnitTests/Pages/Users/Login.cshtml.Test.cs
@@ -4,6 +4,18 @@ using Moq;
 
 using ContosoCrafts.WebSite.Pages;
 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
 
 namespace UnitTests.Pages.Users
 {
@@ -28,8 +40,15 @@ namespace UnitTests.Pages.Users
 
         #region OnPost
         [Test]
-        public void valid_should_do()
+        public void valid_username_valid_password_should_do()
         {
+            int userID = 157465;
+            pageModel.UserInput_test = new UserLoginModel()
+            {
+                username = "lakers34",
+                password = "dscWTr"
+            };
+
             Assert.AreEqual('2', '2');
 
         }
