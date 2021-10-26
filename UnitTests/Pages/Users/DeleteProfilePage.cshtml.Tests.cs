daf8fea17688aa7aaef02007a7f09b35d7057f97
manasaacharya <macharya@seattleu.edu>
2021-10-26 16:29:36 -0700

    MBA: created new unit test in DeleteProfilePage.cshtml.Tests.cs

---
+++ UnitTests/Pages/Users/DeleteProfilePage.cshtml.Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using Moq;
+
+using ContosoCrafts.WebSite.Pages;
+using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTests.Pages.Users
+{
+    class DeleteProfilePage
+    {
+        
+    }
+};
