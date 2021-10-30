1f4713bcdd6ac8c9f9f044a8272cda03ee955096
manasaacharya <macharya@seattleu.edu>
2021-10-30 12:04:50 -0700

    Added comments following guidelines to UpdateUserModel.cs

--- src/Models/UpdateUserModel.cs
+++ src/Models/UpdateUserModel.cs
@@ -6,17 +6,37 @@ using System.Threading.Tasks;
 namespace ContosoCrafts.WebSite.Models
 {
     
-    // -------  Model For Update User --------
+    /// <summary>
+    /// Model for handling the update of users in database user.json
+    /// User must alread exist for update
+    /// </summary>
     public class UpdateUserModel
     {
+        /// <summary>
+        /// get set for updating the unique ID 
+        /// </summary>
         public int UpdateID { get; set; }
 
+        /// <summary>
+        /// get and set for updating string name of user
+        /// </summary>
         public string UpdateName { get; set; }
 
+        /// <summary>
+        /// get and set for updating string email of user
+        /// </summary>
         public string UpdateEmail { get; set; }
 
+        /// <summary>
+        /// get and set of string location of user
+        /// </summary>
         public string UpdateLocation { get; set; }
 
+        /// <summary>
+        /// get and set of updating password of user
+        /// password has requirements in order to be password
+        /// min of 6 characters
+        /// </summary>
         public string UpdatePassword { get; set; }
     }
 }
