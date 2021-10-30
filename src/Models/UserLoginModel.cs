7c16deec811a81262aae0c7d6a3baa7fe350d87e
manasaacharya <macharya@seattleu.edu>
2021-10-30 12:52:56 -0700

    Added comments following guidelines to UserLoginModel.cs

--- src/Models/UserLoginModel.cs
+++ src/Models/UserLoginModel.cs
@@ -6,11 +6,22 @@ using System.Threading.Tasks;
 
 namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// Model for handling user login and user database user.json
+    /// </summary>
     public class  UserLoginModel
     {
+        /// <summary>
+        /// get and set username method for username in userservices
+        /// required field for login 
+        /// </summary>
         [Required(ErrorMessage = "* This field is required")]
         public string username { get; set; }
 
+        /// <summary>
+        /// get and set method for password in userservices 
+        /// required to min 6 characters and field is required for login 
+        /// </summary>
         [Required(ErrorMessage = "* This field is required")]
         [DataType(DataType.Password)]
         [StringLength(50, MinimumLength = 6, ErrorMessage = "* Must be at least 6 characters long")]
