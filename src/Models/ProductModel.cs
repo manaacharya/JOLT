2dd833389c5d0fd4079cd5b501e75b32a26af6b2
Edwin Kaburu <kaburuedwin123@gmail.com>
2021-10-29 17:31:57 -0700

    Added Mike's code for ProductModel in ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoCrafts.WebSite.Models
 {
@@ -7,16 +8,25 @@ namespace ContosoCrafts.WebSite.Models
     {
         public string Id { get; set; }
         public string Maker { get; set; }
-        
+
         [JsonPropertyName("img")]
         public string Image { get; set; }
+
         public string Url { get; set; }
+
+        [StringLength(maximumLength: 33, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
         public string Title { get; set; }
+
         public string Description { get; set; }
+
         public int[] Ratings { get; set; }
 
-        public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
 
- 
+        public string Quantity { get; set; }
+
+        [Range(-1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int Price { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
     }
 }
