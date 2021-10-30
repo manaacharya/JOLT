dbcbd5e644e9a2cdf6ecbb10d4ebe6f45ec0bb67
Edwin Kaburu <kaburuedwin123@gmail.com>
2021-10-29 17:20:53 -0700

    Added Mike's update services functionalities in JsonFileProductService.cs

--- src/Services/JsonFileProductService.cs
+++ src/Services/JsonFileProductService.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace ContosoCrafts.WebSite.Services
 {
-   public class JsonFileProductService
+    public class JsonFileProductService
     {
         public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
         {
@@ -23,9 +23,9 @@ namespace ContosoCrafts.WebSite.Services
             get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
         }
 
-        public IEnumerable<ProductModel> GetProducts()
+        public IEnumerable<ProductModel> GetAllData()
         {
-            using(var jsonFileReader = File.OpenText(JsonFileName))
+            using (var jsonFileReader = File.OpenText(JsonFileName))
             {
                 return JsonSerializer.Deserialize<ProductModel[]>(jsonFileReader.ReadToEnd(),
                     new JsonSerializerOptions
@@ -52,7 +52,7 @@ namespace ContosoCrafts.WebSite.Services
                 return false;
             }
 
-            var products = GetProducts();
+            var products = GetAllData();
 
             // Look up the product, if it does not exist, return
             var data = products.FirstOrDefault(x => x.Id.Equals(productId));
@@ -91,6 +91,35 @@ namespace ContosoCrafts.WebSite.Services
         }
 
         /// <summary>
+        /// Find the data record
+        /// Update the fields
+        /// Save to the data store
+        /// </summary>
+        /// <param name="data"></param>
+        public ProductModel UpdateData(ProductModel data)
+        {
+            var products = GetAllData();
+            var productData = products.FirstOrDefault(x => x.Id.Equals(data.Id));
+            if (productData == null)
+            {
+                return null;
+            }
+
+            // Update the data to the new passed in values
+            productData.Title = data.Title;
+            productData.Description = data.Description.Trim();
+            productData.Url = data.Url;
+            productData.Image = data.Image;
+
+            productData.Quantity = data.Quantity;
+            productData.Price = data.Price;
+
+            SaveData(products);
+
+            return productData;
+        }
+
+        /// <summary>
         /// Save All products data to storage
         /// </summary>
         private void SaveData(IEnumerable<ProductModel> products)
@@ -126,12 +155,30 @@ namespace ContosoCrafts.WebSite.Services
             };
 
             // Get the current set, and append the new record to it becuase IEnumerable does not have Add
-            var dataSet = GetProducts();
+            var dataSet = GetAllData();
             dataSet = dataSet.Append(data);
 
             SaveData(dataSet);
 
             return data;
         }
+
+        /// <summary>
+        /// Remove the item from the system
+        /// </summary>
+        /// <returns></returns>
+        public ProductModel DeleteData(string id)
+        {
+            // Get the current set, and append the new record to it
+            var dataSet = GetAllData();
+            var data = dataSet.FirstOrDefault(m => m.Id.Equals(id));
+
+            var newDataSet = GetAllData().Where(m => m.Id.Equals(id) == false);
+
+            SaveData(newDataSet);
+
+            return data;
+        }
+
     }
 }
