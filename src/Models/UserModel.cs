11c741be004be26ac39cd8b882127b6ac1e7eb46
manasaacharya <macharya@seattleu.edu>
2021-10-30 13:06:24 -0700

    Added comments to UserModel.cs

--- src/Models/UserModel.cs
+++ src/Models/UserModel.cs
@@ -4,36 +4,58 @@ using System.Text.Json.Serialization;
 
 namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// Model for handling user operations and user database users.json
+    /// </summary>
     public class UserModel
     {
-        //This class contains data validation above each attribute
+        /// <summary>
+        /// get and set userId
+        /// unique userId given at random to user when created in userServices
+        /// </summary>
         public int userID { get; set; }
 
+        /// <summary>
+        /// get set string username of user specific to ID 
+        /// </summary>
         [RegularExpression("^[a-zA-Z0-9]+$", ErrorMessage = "* Must only include letters or numbers")]
         [Required(ErrorMessage = "* This field is required")]
         public string username { get; set; }
 
-
+        /// <summary>
+        /// get set password of userID
+        /// required minimum length of 6 characters
+        /// required field 
+        /// </summary>
         [Required(ErrorMessage = "* This field is required")]
         [DataType(DataType.Password)]
         [StringLength(50, MinimumLength = 6, ErrorMessage = "* Must be at least 6 characters long")]
         public string password { get; set; }
 
-        /*[Required(ErrorMessage = "* This field is required")]
-        [Compare("password")]
-        public string confirmPassword { get; set; }*/
-
-
+        /// <summary>
+        /// get set email for userID
+        /// required field 
+        /// </summary>
         [EmailAddress(ErrorMessage = "* Enter a valid email")]
         [RegularExpression("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", ErrorMessage = "* Must be an email address")]
         [DataType(DataType.EmailAddress)]
         [Required(ErrorMessage = "* This field is required")]
         public string email { get; set; }
 
+        /// <summary>
+        /// get and set location for userID
+        /// required field
+        /// can only use characters 
+        /// </summary>
         [RegularExpression("^[a-zA-Z' ']+$", ErrorMessage = "* Location can only have letters")]
         [Required(ErrorMessage = "* This field is required")]
         public string location { get; set; }
 
+        /// <summary>
+        /// toString methof of user object
+        /// includes userID, username, password, email and location
+        /// </summary>
+        /// <returns></returns>
         public override string ToString() => JsonSerializer.Serialize<UserModel>(this);
 
  
