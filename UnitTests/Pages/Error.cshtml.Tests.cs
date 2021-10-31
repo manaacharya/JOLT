2b21f79193fd801902c963a4003a9b3f90e0f705
Edwin Kaburu <kaburuedwin123@gmail.com>
2021-10-31 13:35:12 -0700

    Added comments in Error.cshtml.Tests.cs

--- UnitTests/Pages/Error.cshtml.Tests.cs
+++ UnitTests/Pages/Error.cshtml.Tests.cs
@@ -10,19 +10,30 @@ using ContosoCrafts.WebSite.Pages;
 
 namespace UnitTests.Pages.Error
 {
+    /// <summary>
+    /// ErrorTests Class for Error Page
+    /// </summary>
     public class ErrorTests
     {
         #region TestSetup
+        // Error Model static field/attribute
         public static ErrorModel PageModel;
 
+        /// <summary>
+        /// Test Initialization for Error Page Test
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
+            // logging attribute created
             var MockLoggerDirect = Mock.Of<ILogger<ErrorModel>>();
 
+            // Error Model instance created with logging attribute passed in constructor
             PageModel = new ErrorModel(MockLoggerDirect)
             {
+                // Set the Page Context
                 PageContext = TestHelper.PageContext,
+                // Set the Temp Data
                 TempData = TestHelper.TempData,
             };
         }
@@ -30,36 +41,43 @@ namespace UnitTests.Pages.Error
         #endregion TestSetup
 
         #region OnGet
+
+        /// <summary>
+        /// Test for OnGet() Valid Activity in Error Page
+        /// </summary>
         [Test]
         public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
         {
-            // Arrange
+            // ----------------- Arrange -----------------
 
             Activity activity = new Activity("activity");
             activity.Start();
 
-            // Act
+            // ----------------- Act -----------------
             PageModel.OnGet();
 
-            // Reset
+            // ----------------- Reset -----------------
             activity.Stop();
 
-            // Assert
+            // ----------------- Assert -----------------
             Assert.AreEqual(true, PageModel.ModelState.IsValid);
             Assert.AreEqual(activity.Id, PageModel.RequestId);
         }
 
+        /// <summary>
+        /// Test for OnGet() InValid Activity 
+        /// </summary>
         [Test]
         public void OnGet_InValid_Activity_Null_Should_Return_TraceIdentifier()
         {
-            // Arrange
+            // ----------------- Arrange -----------------
 
-            // Act
+            // ----------------- Act -----------------
             PageModel.OnGet();
 
-            // Reset
+            // ----------------- Reset -----------------
 
-            // Assert
+            // ----------------- Assert -----------------
             Assert.AreEqual(true, PageModel.ModelState.IsValid);
             Assert.AreEqual("trace", PageModel.RequestId);
             Assert.AreEqual(true, PageModel.ShowRequestId);
