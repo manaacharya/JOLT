2952209f5df229ff2618fca7d06f58387057e7dd
Edwin Kaburu <kaburuedwin123@gmail.com>
2021-10-31 13:27:43 -0700

    Added Comments to the Privacy.cshtml.Tests.cs

--- UnitTests/Pages/Privacy.cshtml.Tests.cs
+++ UnitTests/Pages/Privacy.cshtml.Tests.cs
@@ -10,16 +10,26 @@ using ContosoCrafts.WebSite.Pages;
 
 namespace UnitTests.Pages.Privacy
 {
+    /// <summary>
+    /// PrivacyTests Class For Privacy Page 
+    /// </summary>
     public class PrivacyTests
     {
         #region TestSetup
+
+        // Privacy Model static field/attribute
         public static PrivacyModel PageModel;
 
+        /// <summary>
+        ///  Test Initialization for Privacy Page
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
+            // logging attribute created
             var MockLoggerDirect = Mock.Of<ILogger<PrivacyModel>>();
 
+            // Privacy Model instance created with logging attribute passed in constructor
             PageModel = new PrivacyModel(MockLoggerDirect)
             {
                 PageContext = TestHelper.PageContext,
@@ -30,17 +40,21 @@ namespace UnitTests.Pages.Privacy
         #endregion TestSetup
 
         #region OnGet
+
+        /// <summary>
+        /// Test for OnGet 
+        /// </summary>
         [Test]
         public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
         {
-            // Arrange
+            // ----------------- Arrange -----------------
 
             // Act
             PageModel.OnGet();
 
-            // Reset
+            // ----------------- Reset -----------------
 
-            // Assert
+            // ----------------- Assert -----------------
             Assert.AreEqual(true, PageModel.ModelState.IsValid);
         }
 
