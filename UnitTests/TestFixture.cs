e9db6784531ae5448456aedac7cc83d834a123b3
Mustafa <m-thuraya@digitalfuture.sa>
2021-10-30 19:54:52 -0700

    Changed attributes to camelCase in TextFixture.cs

--- UnitTests/TestFixture.cs
+++ UnitTests/TestFixture.cs
@@ -28,27 +28,27 @@ namespace UnitTests
 
 
             // var DataWebPath = "../../../../src/bin/Debug/net5.0/wwwroot/data";
-            var DataWebPath = "../../../../src/wwwroot/data";
-            var DataUTDirectory = "wwwroot";
-            var DataUTPath = DataUTDirectory + "/data";
+            var dataWebPath = "../../../../src/wwwroot/data";
+            var dataUTDirectory = "wwwroot";
+            var dataUTPath = dataUTDirectory + "/data";
 
             // Delete the Detination folder
-            if (Directory.Exists(DataUTDirectory))
+            if (Directory.Exists(dataUTDirectory))
             {
-                Directory.Delete(DataUTDirectory, true);
+                Directory.Delete(dataUTDirectory, true);
             }
 
             // Make the directory
-            Directory.CreateDirectory(DataUTPath);
+            Directory.CreateDirectory(dataUTPath);
 
             // Copy over all data files
-            var filePaths = Directory.GetFiles(DataWebPath);
+            var filePaths = Directory.GetFiles(dataWebPath);
             foreach (var filename in filePaths)
             {
-                string OriginalFilePathName = filename.ToString();
-                var newFilePathName = OriginalFilePathName.Replace(DataWebPath, DataUTPath);
+                string originalFilePathName = filename.ToString();
+                var newFilePathName = originalFilePathName.Replace(dataWebPath, dataUTPath);
 
-                File.Copy(OriginalFilePathName, newFilePathName);
+                File.Copy(originalFilePathName, newFilePathName);
             }
         }
 
