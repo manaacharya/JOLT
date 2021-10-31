7bc394178a5129326cd9f39dd046910f579678d7
Edwin Kaburu <kaburuedwin123@gmail.com>
2021-10-31 13:58:51 -0700

    Added comments to IndexUsersPage.cshtml.Tests.cs

--- UnitTests/Pages/Users/IndexUsersPage.cshtml.Tests.cs
+++ UnitTests/Pages/Users/IndexUsersPage.cshtml.Tests.cs
@@ -9,17 +9,25 @@ using ContosoCrafts.WebSite.Pages;
 
 namespace UnitTests.Pages.Users
 {
+    /// <summary>
+    /// IndexUsersPage for Users Page
+    /// </summary>
     class IndexUsersPage
     {
         #region TestSetup
-
+        // Users Page Model static field/attribute
         public static UsersPageModel UsersPageModel;
 
+        /// <summary>
+        /// Test Initialization for UsersPage 
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
+            // logging attribute created
             var MockLoggerDirect = Mock.Of<ILogger<UsersPageModel>>();
 
+            // UsersPage Model instance created with logging attribute passed in constructor
             UsersPageModel = new UsersPageModel(MockLoggerDirect, TestHelper.UserService)
             {
             };
@@ -28,15 +36,21 @@ namespace UnitTests.Pages.Users
         #endregion TestSetup
 
         #region OnGet
+
+        /// <summary>
+        /// Test for OnGet() Collection of Users
+        /// </summary>
         [Test]
         public void OnGet_Valid_Should_Return_Users()
         {
-            // Arrange
+            // ----------------- Arrange -----------------
 
-            // Act
+            // ----------------- Act -----------------
+
+            // Fetch result from OnGet()
             UsersPageModel.OnGet();
 
-            // Assert
+            // ----------------- Assert -----------------
             Assert.AreEqual(true, UsersPageModel.ModelState.IsValid);
             Assert.AreEqual(true, UsersPageModel.Users.ToList().Any());
         }
