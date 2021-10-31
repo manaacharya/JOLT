059606459f845fdeeedcd580f34f7e780acd0e0a
Edwin Kaburu <kaburuedwin123@gmail.com>
2021-10-31 13:30:30 -0700

    Added comments for Index.cshtml.Tests.cs

--- UnitTests/Pages/Index.cshtml.Tests.cs
+++ UnitTests/Pages/Index.cshtml.Tests.cs
@@ -10,17 +10,26 @@ using ContosoCrafts.WebSite.Pages;
 
 namespace UnitTests.Pages.Index
 {
+    /// <summary>
+    /// IndexTests Class for Index Page
+    /// </summary>
     public class IndexTests
     {
         #region TestSetup
 
+        // Index Model static field/attribute
         public static IndexModel PageModel;
 
+        /// <summary>
+        /// Test Initialization for Index Page Test
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
+            // logging attribute created
             var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
 
+            // Index Model instance created with logging attribute passed in constructor
             PageModel = new IndexModel(MockLoggerDirect, TestHelper.ProductService)
             {
             };
@@ -29,15 +38,19 @@ namespace UnitTests.Pages.Index
         #endregion TestSetup
 
         #region OnGet
+
+        /// <summary>
+        /// Test for OnGet() Valid Products
+        /// </summary>
         [Test]
         public void OnGet_Valid_Should_Return_Products()
         {
-            // Arrange
+            // ----------------- Arrange -----------------
 
-            // Act
+            // ----------------- Act -----------------
             PageModel.OnGet();
 
-            // Assert
+            // ----------------- Assert -----------------
             Assert.AreEqual(true, PageModel.ModelState.IsValid);
             Assert.AreEqual(true, PageModel.Products.ToList().Any());
         }
