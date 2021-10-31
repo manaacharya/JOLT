3082db635f8c8b73054a4247d95de54132f910f3
Edwin Kaburu <kabuuredwin123@gmail.com>
2021-10-30 20:38:32 -0700

    Added Read unit test for UserModel OnGet in ReadProfilePage.cshtml.Tests.cs

--- UnitTests/Pages/Users/ReadProfilePage.cshtml.Tests.cs
+++ UnitTests/Pages/Users/ReadProfilePage.cshtml.Tests.cs
@@ -1,12 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using Moq;
+
+using ContosoCrafts.WebSite.Pages;
+using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace UnitTests.Pages.Users
 {
     class ReadProfilePage
     {
+
+        #region TestSetup
+        public static ProfilePageModel PageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<ProfilePageModel>>();
+
+            PageModel = new ProfilePageModel(MockLoggerDirect, TestHelper.UserService)
+            {
+                PageContext = TestHelper.PageContext
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+
+        [Test]
+        public void OnGet_Valid_Should_Return_UserModel()
+        {
+            // Arrange
+            string key = "lakers34";
+
+            TestHelper.UserService.CreateCookie("nameCookie", key);
+
+            // Act
+
+            PageModel.OnGet();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, PageModel.ModelState.IsValid);
+            Assert.AreEqual("lakers34", PageModel.UserModel.Username);
+            Assert.AreEqual("Egypt", PageModel.UserModel.Location);
+
+        }
+
+        #endregion OnGet
+
+
     }
 }
