b84bfc6afe2f4a22f4629c9027d906127936a4e7
Edwin Kaburu <kaburuedwin123@gmail.com>
2021-10-31 14:03:56 -0700

    Added comments in DeleteProfilePage.cshtml.Tests.cs

--- UnitTests/Pages/Users/DeleteProfilePage.cshtml.Tests.cs
+++ UnitTests/Pages/Users/DeleteProfilePage.cshtml.Tests.cs
@@ -10,18 +10,29 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace UnitTests.Pages.Users
 {
+    /// <summary>
+    /// DeleteProfilePage for Delete
+    /// </summary>
     class DeleteProfilePage
     {
         #region TestSetup
+
+        // ProfilePage Model static field/attribute
         public static ProfilePageModel PageModel;
 
+        /// <summary>
+        /// Test Initialization for ProfilePage
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
+            // logging attribute created
             var MockLoggerDirect = Mock.Of<ILogger<ProfilePageModel>>();
 
+            // Profile Model instance created with logging attribute passed in constructor
             PageModel = new ProfilePageModel(MockLoggerDirect, TestHelper.UserService)
             {
+                // Set the Page Context
                 PageContext = TestHelper.PageContext
             };
         }
@@ -29,48 +40,47 @@ namespace UnitTests.Pages.Users
         #endregion TestSetup 
 
         #region OnPostDeleteProfile
+        /// <summary>
+        /// Test for OnPost(), Delete Valid UserModel
+        /// </summary>
         [Test]
         public void OnPost_Valid_UserModel_Should_Delete_UserObject()
         {
-            // Arrange
-            //valid ID 
+            // ----------------- Arrange -----------------
+            // Valid id variable
             int userID = 343386;
-            PageModel.PageContext.HttpContext.Response.Cookies.Append("nameCookie", "craigs34");
-
-            // Act
+            
+            // ----------------- Act -----------------
+            // Fetch result from OnPost
             var result = PageModel.OnPostDeleteProfile(userID) as RedirectToPageResult;
 
-            // Assert
+            // ----------------- Assert -----------------
             Assert.AreEqual(true, PageModel.ModelState.IsValid);
-            //var name = result.PageName;
-            //Assert.AreEqual(true, result.PageName.Contains("Index"));
             Assert.AreEqual(null, PageModel.PageContext.HttpContext.Request.Cookies["nameCookie"]);
 
-            //confirm
-
-            Assert.AreEqual("User deleted.", PageModel.Message); 
-
             // Confirm the item is deleted
-          
-            
+            Assert.AreEqual("User deleted.", PageModel.Message); 
         }
 
+        /// <summary>
+        /// Test for OnPost(), Delete InValid UserModel
+        /// </summary>
         [Test]
         public void OnPost_InValid_UserModel_Should_Return_Page()
         {
-            // Arrange
+            // ----------------- Arrange -----------------
 
             int invalidId = -93939;
 
-            // Act
+            // ----------------- Act -----------------
+            // Fetch result from OnPost
             var result = PageModel.OnPostDeleteProfile(invalidId) as RedirectToPageResult;
 
-            // Reset
+            // ----------------- Reset -----------------
 
-            // Assert
+            // ----------------- Assert -----------------
             Assert.AreEqual("ProfilePage", result.PageName);
             Assert.AreEqual("User Not Deleted", PageModel.Message);
-
 
         }
 
