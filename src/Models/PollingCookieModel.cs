45f51378b8e9c7652ac4cf6660bc888c6a11c375
Edwin Kaburu <kaburuedwin123@gmail.com>
2021-10-31 12:41:01 -0700

    Comments added in PollingCookieModel.cs

--- src/Models/PollingCookieModel.cs
+++ src/Models/PollingCookieModel.cs
@@ -5,8 +5,14 @@ using System.Threading.Tasks;
 
 namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// Cookie Class for Polling
+    /// </summary>
     public class PollingCookieModel
     {
+        /// <summary>
+        /// Dictionary collection of key-value pairs
+        /// </summary>
         public Dictionary<string, string> CookieCollection { get; set; }
     }
 }
