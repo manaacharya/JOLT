e94e6333b2b45d9c18e4157e8bb7fb7e4cf68657
Edwin Kaburu <kabuuredwin123@gmail.com>
2021-10-30 18:21:50 -0700

    Create new model file and added attribute in PollingCookieModel.cs

---
+++ src/Models/PollingCookieModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class PollingCookieModel
+    {
+        public Dictionary<string, string> CookieCollection { get; set; }
+    }
+}
