6c0b921314ad423c01bb5f1134f36c2f82a41bb4
Fangsheng Xu <fangcyxu@uw.edu>
2021-10-31 10:44:05 -0700

    Added LoginWeolcomePage.cshtml.Test.cs under unit test/pages.user

---
+++ UnitTests/Pages/Users/LoginWelcomePage.cshtml.Test.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using Moq;
+
+using ContosoCrafts.WebSite.Pages;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UnitTests.Pages.Users
+{
+    class LoginWelcomePageTest
+    {
+        #region
+        public static Login_WelcomeModel LoginWelcomeModel;
+
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<Login_WelcomeModel>>();
+
+        }
+        #endregion
+    }
+}
