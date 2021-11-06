4fbd7bfb8567eedd248beb2ac7a1750c9d3d0ecc
Edwin Kaburu <kaburuedwin123@gmail.com>
2021-11-06 15:43:50 -0700

    Unit test file for Poll Creation created in CreatePollPage.cshtml.Test.cs

---
+++ UnitTests/Pages/Polls/CreatePollPage.cshtml.Test.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using Moq;
+
+using ContosoCrafts.WebSite.Pages;
+using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using ContosoCrafts.WebSite.Pages.PollsPages;
+
+namespace UnitTests.Pages.Polls
+{
+    class CreatePollPage
+    {
+        #region TestSetup
+
+        // CreatePollPage Model static field/attribute
+        public static CreatePollPageModel PageModel;
+
+        /// <summary>
+        /// Test Initialization for ProfilePage
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            // logging attribute created
+            var MockLoggerDirect = Mock.Of<ILogger<ProfilePageModel>>();
+
+            // Profile Model instance created with logging attribute passed in constructor
+           /* PageModel = new ProfilePageModel(MockLoggerDirect, TestHelper.UserService)
+            {
+                // Set the Page Context
+                PageContext = TestHelper.PageContext
+            };*/
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        #endregion OnGet
+
+        #region OnPost
+        #endregion OnPost
+
+    }
+}
