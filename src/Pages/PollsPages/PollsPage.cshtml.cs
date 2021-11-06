008e9ddb63b73b92bedc6da1250c909afe170726
Mustafa <m-thuraya@digitalfuture.sa>
2021-11-05 23:17:51 -0700

    Add onGet() for polls in PollsPage.cshtml.cs

--- src/Pages/PollsPages/PollsPage.cshtml.cs
+++ src/Pages/PollsPages/PollsPage.cshtml.cs
@@ -4,21 +4,48 @@ using Microsoft.Extensions.Logging;
 
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
+using System.Collections.Generic;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// PollPage model for displaying polls
+    /// </summary>
     public class PollsPageModel : PageModel
     {
         // logger logs the information ex system info, error
         private readonly ILogger<PollsPageModel> _logger;
 
         /// <summary>
-        /// initializes the logger and userService
+        /// Poll Services
+        /// </summary>
+        public JsonFilePollService PollServices { get; set; }
+
+        /// <summary>
+        /// List of Polls
+        /// </summary>
+        public IEnumerable<PollModel> Polls { get; set; }
+
+        /// <summary>
+        /// initializes the logger and pollService
         /// </summary>
         /// <param name="logger"></param>
-        public PollsPageModel(ILogger<PollsPageModel> logger)
+        /// <param name="pollService"></param>
+        public PollsPageModel(ILogger<PollsPageModel> logger,
+            JsonFilePollService pollService)
         {
             _logger = logger;
+
+            //Pollservice object 
+            PollServices = pollService;
+        }
+
+        /// <summary>
+        /// initializes a list of all
+        /// </summary>
+        public void OnGet()
+        {
+            Polls = PollServices.GetPolls();
         }
 
         /// <summary>
