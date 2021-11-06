ec91c86d9a30db236961bb5da928aa697f259114
Mustafa <m-thuraya@digitalfuture.sa>
2021-11-05 18:35:10 -0700

    Spaced out lines of code in Register.cshtml.cs

--- src/Pages/UserPages/Register.cshtml.cs
+++ src/Pages/UserPages/Register.cshtml.cs
@@ -34,10 +34,12 @@ namespace ContosoCrafts.WebSite.Pages
         // The data to show, bind to it for the post
         public RegisterModel(ILogger<RegisterModel> logger, JsonFileUserService userService)
         {
+
             _logger = logger;
 
             //userservice object 
             UserService = userService;
+
         }
 
         /// <summary>
@@ -69,25 +71,33 @@ namespace ContosoCrafts.WebSite.Pages
             //use regular expression to check if username contains only number or letters
             if (usernameRg.IsMatch(BindUser.Username) == false)
             {
+
                 return Page();
+
             }
 
             //check if password length is less than 6
             if (BindUser.Password.Length < 6)
             {
+
                 return Page();
+
             }
 
             //check if email is not valid
             if (emailRg.IsMatch(BindUser.Email) == false)
             {
+
                 return Page();
+
             }
 
             //check if location is not valid
             if (locationRg.IsMatch(BindUser.Location) == false)
             {
+
                 return Page();
+
             }
 
             //create data using userinformation 
@@ -95,11 +105,12 @@ namespace ContosoCrafts.WebSite.Pages
 
             //new nameCookie to username 
             UserService.CreateCookie("nameCookie", BindUser.Username);
+
             //Response.Cookies.Append("nameCookie", BindUser.Username);
 
             return RedirectToPage("./ProfilePage");
         }
 
-
     }
+
 }
