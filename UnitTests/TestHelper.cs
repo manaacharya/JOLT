cd004c501fd2d1b369bdc9d19f38ffcc755e922e
Mustafa <m-thuraya@digitalfuture.sa>
2021-11-05 19:27:20 -0700

    Spaced out lines of code in TestHelper.cs

--- UnitTests/TestHelper.cs
+++ UnitTests/TestHelper.cs
@@ -34,15 +34,25 @@ namespace UnitTests
     public static class TestHelper
     {
         public static Mock<IWebHostEnvironment> MockWebHostEnvironment;
+
         public static IUrlHelperFactory UrlHelperFactory;
+
         public static DefaultHttpContext HttpContextDefault;
+
         public static IWebHostEnvironment WebHostEnvironment;
+
         public static ModelStateDictionary ModelState;
+
         public static ActionContext ActionContext;
+
         public static EmptyModelMetadataProvider ModelMetadataProvider;
+
         public static ViewDataDictionary ViewData;
+
         public static TempDataDictionary TempData;
+
         public static PageContext PageContext;
+
         // public static JsonFileProductService ProductService;
 
         // User Services
@@ -54,14 +64,19 @@ namespace UnitTests
         static TestHelper()
         {
             MockWebHostEnvironment = new Mock<IWebHostEnvironment>();
+
             MockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
+
             MockWebHostEnvironment.Setup(m => m.WebRootPath).Returns(TestFixture.DataWebRootPath);
+
             MockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns(TestFixture.DataContentRootPath);
+
 
             HttpContextDefault = new DefaultHttpContext()
             {
                 TraceIdentifier = "trace",
             };
+
             HttpContextDefault.HttpContext.TraceIdentifier = "trace";
 
             ModelState = new ModelStateDictionary();
@@ -69,12 +84,15 @@ namespace UnitTests
             ActionContext = new ActionContext(HttpContextDefault, HttpContextDefault.GetRouteData(), new PageActionDescriptor(), ModelState);
 
             ModelMetadataProvider = new EmptyModelMetadataProvider();
+
             ViewData = new ViewDataDictionary(ModelMetadataProvider, ModelState);
+
             TempData = new TempDataDictionary(HttpContextDefault, Mock.Of<ITempDataProvider>());
 
             PageContext = new PageContext(ActionContext)
             {
                 ViewData = ViewData,
+
                 HttpContext = HttpContextDefault
             };
 
