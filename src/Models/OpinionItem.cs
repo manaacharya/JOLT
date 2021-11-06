cdd809d9b3ca70f219654aae21459ac83f0e012c
Edwin Kaburu <kabuuredwin123@gmail.com>
2021-11-05 22:08:30 -0700

    Added Default constructor in OpinionItem.cs

--- src/Models/OpinionItem.cs
+++ src/Models/OpinionItem.cs
@@ -19,6 +19,8 @@ namespace ContosoCrafts.WebSite.Models
         /// </summary>
         public int NumCounts { get; set; }
 
+        public OpinionItem() { }
+
         /// <summary>
         /// OpinionItem constructor 
         /// </summary>
