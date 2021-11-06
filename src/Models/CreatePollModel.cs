4cd504fb541db0208e92ff7e96f9158063c0d658
Edwin Kaburu <kabuuredwin123@gmail.com>
2021-11-05 20:31:28 -0700

    Add creation attributes for PollModel in CreatePollModel.cs

---
+++ src/Models/CreatePollModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class CreatePollModel
+    {
+        public string CreateTitle { get; set; }
+
+        public string UpdateDescription { get; set; }
+
+        public string OpinionOne { get; set; }
+
+        public string OpinionTwo { get; set; }
+    }
+}
