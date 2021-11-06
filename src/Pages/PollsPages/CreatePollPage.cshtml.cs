0e52a06a786f7a08ae00e06a82683f0ae9de9094
Edwin Kaburu <kabuuredwin123@gmail.com>
2021-11-05 20:41:19 -0700

    Add attributes for the poll creations page in CreatePollPage.cshtml.cs

--- src/Pages/PollsPages/CreatePollPage.cshtml.cs
+++ src/Pages/PollsPages/CreatePollPage.cshtml.cs
@@ -4,13 +4,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages.PollsPages
 {
     public class CreatePollPageModel : PageModel
     {
+        public string Message { get; set; }
+
+        private readonly ILogger<CreatePollPageModel> _logger;
+
+        public JsonFileUserService UserServices { get; set; }
+
+        public JsonFilePollService PollService { get; set; }
+
+        [BindProperty]
+        public CreatePollModel CreatePoll { get; set; }
+
+        public CreatePollPageModel(ILogger<CreatePollPageModel> logger,
+            JsonFileUserService userService, JsonFilePollService pollService)
+        {
+            _logger = logger;
+            UserServices = userService;
+            PollService = pollService;
+        }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+         //   var pollCreationStatus = PollService.CreatePoll
+
+            return RedirectToPage("PollsPage");
         }
     }
 }
