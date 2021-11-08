53ea0fced4825546a8720f1ea40030262e568bf8
manasaacharya <macharya@seattleu.edu>
2021-11-08 11:48:56 -0800

    Deleted unused code in AboutUs.cshtml.cs

--- src/Pages/AboutUs.cshtml.cs
+++ src/Pages/AboutUs.cshtml.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
@@ -18,25 +13,6 @@ namespace ContosoCrafts.WebSite.Pages
         // create log category of AboutUsModel 
         private readonly ILogger<AboutUsModel> _logger;
 
-        /// <summary>
-        /// initialize the logger object
-        /// </summary>
-        /// <param name="logger"></param> logger object
-         
-        /*
-        public AboutUsModel(ILogger<AboutUsModel> logger)
-        {
-            _logger = logger;
-        }
-        */
-        /// <summary> 
-        /// This is a dummy method for now
-        /// </summary>
-        /*
-        public void OnGet()
-        {
-        }
-        */
     }
 
 }
