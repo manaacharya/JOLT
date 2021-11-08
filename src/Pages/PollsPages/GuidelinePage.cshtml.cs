abbf17cafbeb7d8c9300141e17a95f29025af88d
manasaacharya <macharya@seattleu.edu>
2021-11-08 11:47:00 -0800

    Deleted unused code in GuidelinePage.cshtml.cs

--- src/Pages/PollsPages/GuidelinePage.cshtml.cs
+++ src/Pages/PollsPages/GuidelinePage.cshtml.cs
@@ -1,34 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
 
     public class GuidelinePage : PageModel
     {
-        /*
-        // logger logs the information ex system info, error
-        private readonly ILogger<GuidelinePage> _logger;
-
-        /// <summary>
-        /// initializes the logger
-        /// </summary>
-        /// <param name="logger"></param>
-        /*
-        public GuidelinePage(ILogger<GuidelinePage> logger)
-        {
-            _logger = logger;
-        }
-        */
-
-        /// <summary>
-        /// OnGet - no action just a page with text displayed 
-        /// </summary>
-        /*
-        public void OnGet()
-        {
-        }
-        */
+     
     }
 
 }
