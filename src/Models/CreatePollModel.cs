e0f5430ab7d2dcea201ccdcf2ad6010b6932e8fc
manasaacharya <macharya@seattleu.edu>
2021-11-08 11:19:27 -0800

    Added comments in CreatePollModel.cs

--- src/Models/CreatePollModel.cs
+++ src/Models/CreatePollModel.cs
@@ -5,14 +5,30 @@ using System.Threading.Tasks;
 
 namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// Create poll model, has title, description and multiple opinions
+    /// </summary>
     public class CreatePollModel
     {
+
+        /// <summary>
+        /// Method to create title of poll
+        /// </summary>
         public string CreateTitle { get; set; }
 
+        /// <summary>
+        /// Method to create the description of poll
+        /// </summary>
         public string CreateDescription { get; set; }
 
+        /// <summary>
+        /// method to create an opinion to be voted on in poll
+        /// </summary>
         public string CreateOpinionOne { get; set; }
 
+        /// <summary>
+        /// method to create an opinion to be voted on in poll
+        /// </summary>
         public string CreateOpinionTwo { get; set; }
     }
 }
