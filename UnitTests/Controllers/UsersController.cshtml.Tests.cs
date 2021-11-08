d5c8588c10766b81ddb9153cde5654f9baddfde2
manasaacharya <macharya@seattleu.edu>
2021-11-08 11:57:21 -0800

    Fixed spacing and UT comments in UsersController.cshtml.Tests.cs

--- UnitTests/Controllers/UsersController.cshtml.Tests.cs
+++ UnitTests/Controllers/UsersController.cshtml.Tests.cs
@@ -9,50 +9,48 @@ namespace UnitTests.Controllers.Users
     /// </summary>
     class UserControllerTest
     { 
-    /// <summary>
-    /// Test UserController for all user services methods 
-    /// </summary>
-    #region TestSetup
-    // UsersController static field/attribute
-    public static UsersController usersController;
+
+        #region TestSetup
+        // UsersController static field/attribute
+        public static UsersController usersController;
  
         /// <summary>
         /// Test Initialization for UsersController 
         /// </summary>
         [SetUp]
-    public void TestInitialize()
-    {
+        public void TestInitialize()
+        {
         // Usercontroller instance created
         // with UserService passsed through constructor 
         usersController = new UsersController(TestHelper.UserService); 
         
-    }
-
-    #endregion TestSetup
-
-    #region Get
-
-    /// <summary>
-    /// Test for OnGet() Collection of Users
-    /// </summary>
-    [Test]
-    public void Get_Valid_Should_Return_Users()
-    {
-        //Arrange 
-
-        //Act
-
-        // Fetch result from OnGet()
-        var Users = usersController.Get();
-      
-        //Assert
-
-        //check model state 
-        Assert.AreEqual(true, usersController.ModelState.IsValid);
-
-        //check User object  
-        Assert.AreEqual(true, usersController.Users.ToList().Any());
         }
-    #endregion Get
-  }
+
+        #endregion TestSetup
+
+        #region Get
+
+        /// <summary>
+        /// Test for OnGet() Collection of Users
+        /// </summary>
+        [Test]
+        public void Get_Valid_Should_Return_Users()
+        {
+            //Arrange 
+
+            //Act
+
+            // Fetch result from OnGet()
+            var Users = usersController.Get();
+      
+            //Assert
+
+            //check model state 
+            Assert.AreEqual(true, usersController.ModelState.IsValid);
+
+            //check User object  
+            Assert.AreEqual(true, usersController.Users.ToList().Any());
+            }
+        #endregion Get
+    }
 }
