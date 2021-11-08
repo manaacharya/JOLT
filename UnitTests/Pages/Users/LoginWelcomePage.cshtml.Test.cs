d9a9066faeb81e4d7a1c9c7bd5cadfadc6edc047
manasaacharya <macharya@seattleu.edu>
2021-11-08 12:13:23 -0800

    Deleted unused code in LoginWelcomePage.cshtml.Test.cs

--- UnitTests/Pages/Users/LoginWelcomePage.cshtml.Test.cs
+++ UnitTests/Pages/Users/LoginWelcomePage.cshtml.Test.cs
@@ -1,38 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
-using NUnit.Framework;
-using Moq;
-
-using ContosoCrafts.WebSite.Pages;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-
-using ContosoCrafts.WebSite.Models;
-using ContosoCrafts.WebSite.Services;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
+﻿
 namespace UnitTests.Pages.Users
 {
     class LoginWelcomePageTest
     {
         #region
-        // public static Login_WelcomeModel LoginWelcomeModel;
-        /*
-        [SetUp]
-        public void TestInitialize()
-        {
-            // var MockLoggerDirect = Mock.Of<ILogger<Login_WelcomeModel>>();
-
-            // LoginWelcomeModel = new Login_WelcomeModel()
-            // {
-                // PageContext = TestHelper.PageContext
-            // };
-        }
-        */
+      
         #endregion
     }
 }
