825811d2363df24d797b317fd897eadc4afda71e
manasaacharya <macharya@seattleu.edu>
2021-11-08 11:47:53 -0800

    Deleted unused code in Login_Welcome.cshtml.cs

--- src/Pages/UserPages/LoginFolder/Login_Welcome.cshtml.cs
+++ src/Pages/UserPages/LoginFolder/Login_Welcome.cshtml.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages
 {
     public class Login_WelcomeModel : PageModel
     {
-        // public object Username { get; private set; }
-
+        
     }
 }
