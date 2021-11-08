bd524850595d3e193a1aefeeb1eb4a0105f39c9f
manasaacharya <macharya@seattleu.edu>
2021-11-08 11:46:09 -0800

    Deleted unused code in login.cshtml.cs

--- src/Pages/UserPages/LoginFolder/login.cshtml.cs
+++ src/Pages/UserPages/LoginFolder/login.cshtml.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
