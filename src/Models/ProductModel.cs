4eef21d7cd8281e848c0e1f7171d8cfa98734071
Edwin Kaburu <kaburuedwin123@gmail.com>
2021-11-09 19:36:35 -0800

    Restored ProductModel in ProductModel.cs

---
+++ src/Models/ProductModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class ProductModel
+    {
+    }
+}
