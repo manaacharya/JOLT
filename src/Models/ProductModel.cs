55d15984673af5896280069de71abc09a7b25d4b
Edwin Kaburu <kaburuedwin123@gmail.com>
2021-11-09 20:59:32 -0800

    Delete unused code in ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -31,8 +31,6 @@ namespace ContosoCrafts.WebSite.Models
         // Store the Comments entered by the users on this product
         public List<CommentModel> CommentList { get; set; } = new List<CommentModel>();
 
-        //public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
-
-
+        
     }
 }
