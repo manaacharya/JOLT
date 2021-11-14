79f401a9e7d352e0b76856879a1bbc895e76a06e
Edwin Kaburu <kaburuedwin123@gmail.com>
2021-11-14 13:45:00 -0800

    Added comments and refactored code in IndexPollsLists.razor.Tests.cs

--- UnitTests/Component/PollsComponent/IndexPollsLists.razor.Tests.cs
+++ UnitTests/Component/PollsComponent/IndexPollsLists.razor.Tests.cs
@@ -8,10 +8,16 @@ using System.Linq;
 
 namespace UnitTests.Component.PollsComponent
 {
+    /// <summary>
+    /// IndexPollsList Class
+    /// </summary>
     class IndexPollsLists : BunitTestContext
     {
         #region TestSetup
 
+        /// <summary>
+        /// Test Setup Initialization
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -19,49 +25,70 @@ namespace UnitTests.Component.PollsComponent
 
         #endregion TestSetup
 
+        /// <summary>
+        /// Test for Rendering the Polls Content
+        /// </summary>
         [Test]
         public void PollList_Default_Should_Return_Content()
         {
             // Arrange
+
+            // Poll Service Singleton Initiation
             Services.AddSingleton<JsonFilePollService>(TestHelper.PollService);
+            // User Service Singleton Initiation
             Services.AddSingleton<JsonFileUserService>(TestHelper.UserService);
 
             // Act
-            var page = RenderComponent<IndexPollsList>();
 
+            // Page Component Rendering
+            var page = RenderComponent<IndexPollsList>();
             // Get the Cards retrned
             var result = page.Markup;
 
             // Assert
+            
+            // Component Checklist
             Assert.AreEqual(true, result.Contains("Aerospace company"));
         }
 
         #region UpdateOpinionInput
 
+        /// <summary>
+        /// Test for Radio Button Selection
+        /// </summary>
         [Test]
         public void UpdateOpinionInput_Valid_Name_Valid_Id_Should_Update_Values()
         {
             // Arrange
+
+            // Poll Service Singleton Initiation
             Services.AddSingleton<JsonFilePollService>(TestHelper.PollService);
+            // User Service Singleton Initiation
             Services.AddSingleton<JsonFileUserService>(TestHelper.UserService);
 
+            // Page Component Rendering
             var page = RenderComponent<IndexPollsList>();
+            // Get all inputs from html
+            var inputList = page.FindAll("input");
 
-            var radioButtonsList = page.FindAll("input");
-
+            // Initialize Id 
             var id = "Boeing";
-
-            // Find one that matches 
-            var radios = radioButtonsList.First(m => m.Id.Contains(id));
+            // Find one that matches the Id
+            var radios = inputList.First(m => m.Id.Contains(id));
 
             // Act
+
+            // Radio Button Event Action
             radios.Change(id);
 
             // Reset
-
-            // Assert
+            
+            // Get the Page markup after radio button event
             var pageMarkup = page.Markup;
 
+            // Assert
+
+            // Component Checklist
             Assert.AreEqual(true, pageMarkup.Contains("Aerospace companies with the best aircrafts"));
         }
 
@@ -70,69 +97,93 @@ namespace UnitTests.Component.PollsComponent
 
         #region SubmitVote
 
+        /// <summary>
+        /// Test for Poll submission with InValid Poll Id and Valid OpinionName
+        /// </summary>
         [Test]
         public void SubmitVote_Valid_OpinionName_InValid_PollId_Should_Return_False()
         {
             // Arrange
-            // Arrange Poll Service
+
+            // Poll Service Singleton Initiation
             Services.AddSingleton<JsonFilePollService>(TestHelper.PollService);
+            // User Service Singleton Initiation
             Services.AddSingleton<JsonFileUserService>(TestHelper.UserService);
 
-            // Render components for IndexPollsList
+            // Page Component Rendering
             var page = RenderComponent<IndexPollsList>();
 
-            // List of Buttons with buttons
+            // Get all buttons from html
             var buttonsList = page.FindAll("button");
 
-            // Find the Button for Submitting a Poll once clicked on 
+            // Find one that matches the "voteBtn"
             var button = buttonsList.First(x => x.Id.Contains("voteBtn"));
 
             // Act
+
+            // Button Click Event Action
             button.Click();
 
             // Reset
+
+            // Get the Page markup after button event
             var pageMarkup = page.Markup;
 
             // Assert
+
+            // Component Checklist
             Assert.AreEqual(true, pageMarkup.Contains("Aerospace companies with the best aircrafts"));
         }
 
+        /// <summary>
+        /// Test for Poll Submission with Valid PollID and Valid OpinionName
+        /// </summary>
         [Test]
-        public void SubmitVote_Valid_OpinionName_Valid_Id_Should_Count_Vote()
+        public void SubmitVote_Valid_OpinionName_Valid_PollId_Should_Count_Vote()
         {
             // Arrange
-            // Arrange Poll Service
+
+            // Poll Service Singleton Initiation
             Services.AddSingleton<JsonFilePollService>(TestHelper.PollService);
+            // User Service Singleton Initiation
             Services.AddSingleton<JsonFileUserService>(TestHelper.UserService);
 
-            // Render components for IndexPollsList
+            // Page Component Rendering
             var page = RenderComponent<IndexPollsList>();
 
-            // List of Buttons with buttons
+            // Get all buttons from html
             var buttonsList = page.FindAll("button");
 
-            // Find the Button for Submitting a Poll once clicked on 
+            // Find one that matches the "voteBtn"
             var button = buttonsList.First(x => x.Id.Contains("voteBtn"));
 
-            var radioButtonsList = page.FindAll("input");
+            // Get all inputs from html
+            var inputList = page.FindAll("input");
 
+            // Initialize Id 
             var id = "Boeing";
+            // Find one that matches the Id
+            var radios = inputList.First(m => m.Id.Contains(id));
 
-            var radios = radioButtonsList.First(m => m.Id.Contains(id));
-
+            // Radio Button Event Action
             radios.Change(id);
 
             // Act
+
+            // Button Click Event Action
             button.Click();
 
             // Reset
+
+            // Get the Page markup after button event
             var pageMarkup = page.Markup;
 
             // Assert
+
+            // Component Checklist
             Assert.AreEqual(true, pageMarkup.Contains("Aerospace companies with the best aircrafts"));
         }
 
         #endregion SubmitVote
-
     }
 }
