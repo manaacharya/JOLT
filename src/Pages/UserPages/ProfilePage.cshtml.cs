75ffe428104a9d82ae001d30b5dc9e35a6b0d098
manasaacharya <macharya@seattleu.edu>
2021-11-14 11:33:29 -0800

    Deleted extra whitespace in ProfilePage.cshtml.cs

--- src/Pages/UserPages/ProfilePage.cshtml.cs
+++ src/Pages/UserPages/ProfilePage.cshtml.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
 
