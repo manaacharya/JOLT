c8bf3561f9ed2b9e80adfd9f089ef1205b68fe72
Edwin Kaburu <kabuuredwin123@gmail.com>
2021-11-13 19:35:34 -0800

    Update file name for IndexPollsLists.razor.Tests.cs

--- UnitTests/Component/PollsComponent/IndexPollsLists.razor.Tests.cs
+++ UnitTests/Component/PollsComponent/IndexPollsLists.razor.Tests.cs
@@ -8,7 +8,7 @@ using System.Linq;
 
 namespace UnitTests.Component.PollsComponent
 {
-    class IndexPollsList:BunitTestContext
+    class IndexPollsLists:BunitTestContext
     {
         #region TestSetup
 
