1aae058253c0e5c83f83fbcd918152071d033556
Edwin Kaburu <kabuuredwin123@gmail.com>
2021-11-13 19:34:40 -0800

    Added default unit test for setup in IndexPollsList.razor.Tests.cs

--- UnitTests/Component/PollsComponent/IndexPollsList.razor.Tests.cs
+++ UnitTests/Component/PollsComponent/IndexPollsList.razor.Tests.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Bunit;
+using NUnit.Framework;
+using ContosoCrafts.WebSite.Component;
+using Microsoft.Extensions.DependencyInjection;
+using ContosoCrafts.WebSite.Services;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace UnitTests.Component.PollsComponent
 {
-    class IndexPollsList
+    class IndexPollsList:BunitTestContext
     {
+        #region TestSetup
+
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+
+        #endregion TestSetup
+
+
+        [Test]
+        public void PollList_Default_Should_Return_Content()
+        {
+            // Arrange
+            Services.AddSingleton<JsonFilePollService>(TestHelper.PollService);
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+
+            // Act
+            var page = RenderComponent<IndexPollsList>();
+
+            // Get the Cards retrned
+            var result = page.Markup;
+
+            // Assert
+            Assert.AreEqual(true, result.Contains("The Quantified Cactus: An Easy Plant Soil Moisture Sensor"));
+        }
+
+
+
     }
 }
