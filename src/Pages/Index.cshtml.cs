023464cf277b1ed7e62ee1896d368d86f29a8698
manasaacharya <macharya@seattleu.edu>
2021-11-14 11:35:59 -0800

    Deleted unused using statements in Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using ContosoCrafts.WebSite.Models;
