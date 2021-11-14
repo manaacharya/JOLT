c8ea6ed9a40368a661454281836d0b2bc9b042f7
manasaacharya <macharya@seattleu.edu>
2021-11-14 11:34:33 -0800

    Deleted extra whitespace in Register.cshtml.cs

--- src/Pages/UserPages/Register.cshtml.cs
+++ src/Pages/UserPages/Register.cshtml.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
