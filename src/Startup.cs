4851c69d489ea2e1936d85986e8d3e08bbafeacd
manasaacharya <macharya@seattleu.edu>
2021-11-16 19:53:41 -0800

    Added comments in Startup.cs

--- src/Startup.cs
+++ src/Startup.cs
@@ -8,66 +8,96 @@ using Microsoft.Extensions.Hosting;
 
 namespace ContosoCrafts.WebSite
 {
+    /// <summary>
+    /// Class for startup for the website 
+    /// </summary>
     public class Startup
     {
+        /// <summary>
+        /// Set up configuration 
+        /// </summary>
+        /// <param name="configuration"></param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        /// <summary>
+        /// method calls configuration
+        /// </summary>
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime.
+        // Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //add razorpage 
             services.AddRazorPages();
 
+            //add servers
             services.AddServerSideBlazor();
 
+            //add http client
             services.AddHttpClient();
 
+            //add client 
             services.AddControllers();
 
+            //Http context accessor 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
-            services.AddTransient<JsonFileProductService>(); // connect to JsonFileProductService
-            services.AddTransient<JsonFileUserService>(); // connect to JsonFileUserService 
+            // connect to JsonFileProductService
+            services.AddTransient<JsonFileProductService>();
 
+            // connect to JsonFileUserService 
+            services.AddTransient<JsonFileUserService>(); 
+
+            //connect to JsonFilePollServices 
             services.AddTransient<JsonFilePollService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime.
+        // Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //development
             if (env.IsDevelopment())
             {
+                //use exception page
                 app.UseDeveloperExceptionPage();
             }
             else
             {
+                //error 
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days.
+                // You may want to change this for production scenarios,
+                // see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
+            //redirection
             app.UseHttpsRedirection();
 
+            //static files
             app.UseStaticFiles();
 
+            //routing
             app.UseRouting();
 
+            //authorization
             app.UseAuthorization();
 
-            // FX: added to in attempt to fix System.InvalidOperationException in login.cshtml.cs
-            // app.UseSession();
-            // app.UseMvc();
-
+            //endpoints
             app.UseEndpoints(endpoints =>
             {
+                //razorpages 
                 endpoints.MapRazorPages();
 
+                //controllers
                 endpoints.MapControllers();
 
+                //blazorhub
                 endpoints.MapBlazorHub();
 
             });
