b884722879a41b6cd51e15317628a43afb11b85d
manasaacharya <macharya@seattleu.edu>
2021-11-16 19:10:04 -0800

    Added comments in GuidelinePage.cshtml.cs

--- src/Pages/PollsPages/GuidelinePage.cshtml.cs
+++ src/Pages/PollsPages/GuidelinePage.cshtml.cs
@@ -2,7 +2,9 @@
 
 namespace ContosoCrafts.WebSite.Pages
 {
-
+    /// <summary>
+    /// method for guideline page - empty just displays text
+    /// </summary>
     public class GuidelinePage : PageModel
     {
      
