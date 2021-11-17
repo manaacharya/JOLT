8b7ea1e5287d79f76ccb1853c182e03bf67233d5
manasaacharya <macharya@seattleu.edu>
2021-11-16 19:15:54 -0800

    Added comments in Login_Welcome.cshtml.cs

--- src/Pages/UserPages/LoginFolder/Login_Welcome.cshtml.cs
+++ src/Pages/UserPages/LoginFolder/Login_Welcome.cshtml.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Method for login page - empty just for text
+    /// </summary>
     public class Login_WelcomeModel : PageModel
     {
         
