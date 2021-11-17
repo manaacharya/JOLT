b45a903f1263be0b1f81a831e2df8444559cd632
manasaacharya <macharya@seattleu.edu>
2021-11-16 20:54:00 -0800

    Added comments in JsonFileProductServiceTests.cs

--- UnitTests/Services/JsonFileProductServiceTests.cs
+++ UnitTests/Services/JsonFileProductServiceTests.cs
@@ -4,9 +4,14 @@ using System.Linq;
 
 namespace UnitTests.Services
 {
+    /// <summary>
+    /// Test for JsonFileProductServiceTests 
+    /// </summary>
     public class JsonFileProductServiceTests
     {
-
+        /// <summary>
+        /// Test set up 
+        /// </summary>
         #region TestSetup
 
         [SetUp]
@@ -16,6 +21,9 @@ namespace UnitTests.Services
 
         #endregion TestSetup
 
+        /// <summary>
+        /// Test adding rating for an invalid product
+        /// </summary>
         #region AddRating
         [Test]
         public void AddRating_InValid_Product_Null_Should_Return_False()
@@ -29,6 +37,9 @@ namespace UnitTests.Services
             Assert.AreEqual(false, result);
         }
 
+        /// <summary>
+        /// Add rating to an invalid product 
+        /// </summary>
         [Test]
         public void AddRating_InValid_Product_Should_Return_False()
         {
@@ -41,6 +52,9 @@ namespace UnitTests.Services
             Assert.AreEqual(false, result);
         }
 
+        /// <summary>
+        /// test adding an invalid rating below 0  
+        /// </summary>
         [Test]
         public void AddRating_Valid_Product_InValid_Rating_Below_0_Should_Return_False()
         {
@@ -53,6 +67,9 @@ namespace UnitTests.Services
             Assert.AreEqual(false, result);
         }
 
+        /// <summary>
+        /// test adding an invalid rating above 5
+        /// </summary>
         [Test]
         public void AddRating_Valid_Product_InValid_Rating_Above_5_Should_Return_False()
         {
@@ -65,6 +82,9 @@ namespace UnitTests.Services
             Assert.AreEqual(false, result);
         }
 
+        /// <summary>
+        /// Add new rating creates new list 
+        /// </summary>
         [Test]
         public void AddRating_Valid_Product_Valid_Rating_When_Existing_Rating_Is_Null_Should_Return_True()
         {
@@ -77,8 +97,10 @@ namespace UnitTests.Services
             Assert.AreEqual(true, result);
         }
 
-        // ....
-
+        
+        /// <summary>
+        /// Add rating successfully 
+        /// </summary>
         [Test]
         public void AddRating_Valid_Product_Valid_Rating_Valid_Should_Return_True()
         {
@@ -86,20 +108,31 @@ namespace UnitTests.Services
 
             // Get the First data item
             var data = TestHelper.ProductService.GetAllData().First();
+            //count length of rating list 
             var countOriginal = data.Ratings.Length;
 
             // Act
+            //check adding data 
             var result = TestHelper.ProductService.AddRating(data.Id, 5);
+
+            //check adding to list correct
             var dataNewList = TestHelper.ProductService.GetAllData().First();
 
             // Assert
             Assert.AreEqual(true, result);
+
+            //check list length 
             Assert.AreEqual(countOriginal + 1, dataNewList.Ratings.Length);
+
+            //check last item in list 
             Assert.AreEqual(5, dataNewList.Ratings.Last());
         }
 
         #endregion AddRating
 
+        /// <summary>
+        /// test updating data
+        /// </summary>
         #region UpdateData
         [Test]
         public void UpdateData_ValidProductModel_InValidProductID_Should_Return_Null()
@@ -109,7 +142,10 @@ namespace UnitTests.Services
             // Instance of ProductModel
             var product = new ProductModel()
             {
+                //id
                 Id = "fakeproduct",
+
+                //description 
                 Description = "This is a Fake Product"
             };
             // Act
@@ -120,7 +156,7 @@ namespace UnitTests.Services
             // Reset
 
             // Assert
-
+            //null result 
             Assert.AreEqual(null, getResult);
         }
         #endregion UpdateData
