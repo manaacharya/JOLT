761756d34d36f3774ccd0705dd4bf034de0ce251
Edwin Kaburu <kaburuedwin123@gmail.com>
2021-11-23 14:22:15 -0800

    Added backend logic to acess poll services in ReadPollPage.cshtml.cs

--- src/Pages/PollsPages/ReadPollPage.cshtml.cs
+++ src/Pages/PollsPages/ReadPollPage.cshtml.cs
@@ -1,16 +1,58 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ContosoCrafts.WebSite.Pages.PollsPages
 {
     public class ReadPollPageModel : PageModel
     {
-        public void OnGet()
+
+        /// <summary>
+        /// Setting logger
+        /// </summary>
+        private readonly ILogger<IndexModel> _logger;
+
+        /// <summary>
+        /// Poll Services
+        /// </summary>
+        public JsonFilePollService PollServices { get; }
+
+        public JsonFileUserService UserService { get; }
+
+        public PollModel PollModel { get; private set; }
+
+        public string OpinionJsonData { get; private set; }
+
+        public UserModel Author { get; private set; }
+
+
+        public ReadPollPageModel(ILogger<IndexModel> logger, JsonFilePollService jsonFilePollService, JsonFileUserService jsonFileUserService)
         {
+            //create logger
+            _logger = logger;
+
+            //create pollservices 
+            PollServices = jsonFilePollService;
+
+            UserService = jsonFileUserService;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            if (id < 0)
+            {
+                return RedirectToPage("/Index");
+            }
+
+            PollModel = PollServices.GetPoll(id);
+
+            OpinionJsonData = PollServices.GetOpinionsJsons(PollModel.OpinionItems);
+
+            Author = UserService.GetUser(PollModel.UserID);
+
+            return Page();
         }
     }
 }
